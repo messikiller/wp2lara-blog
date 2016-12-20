b193498b8e2a5fb2b1f31bced036b737202e1a98
heqiming <messikiller@aliyun.com>
2016-12-20 17:43:04 +0800

    optimize box style

--- design.md
+++ design.md
@@ -12,4 +12,4 @@ dark-gray: #99a1a7
 
 background-gray: #eff3f5
 
-box-shadow: 0px -1px 3px 2px rgba(168, 168, 168, 0.14)
+box-shadow: 0 0 12px rgba(0, 0, 0, 0.1);
--- resources/views/home/boxes.blade.php
+++ resources/views/home/boxes.blade.php
@@ -1,8 +1,10 @@
 <div id="boxes">
+
+@for($i=1; $i<=10; $i++)
     <div class="box">
 
         <div class="box-header">
-            他们的文字中，体会到自己的人生
+            <a href="#">他们的文字中，体会到自己的人生</a>
         </div>
 
         <div class="box-divider"></div>
@@ -14,19 +16,21 @@
 
         <div class="box-footer">
             <div class="box-meta-container">
-                <span class="box-meta"><i class="fa fa-clock-o"></i>&nbsp;三天前</span>
-                <span class="box-meta"><i class="fa fa-eye"></i>&nbsp;56</span>
                 <span class="box-meta">
-                    <i class="fa fa-tags"></i>&nbsp;
+                    <i class="fa fa-tags"></i>&nbsp;标签：
                     <span class="label label-badge label-primary">Primary</span>
                     <span class="label label-badge label-success">Success</span>
                     <span class="label label-badge label-info">Info</span>
                     <span class="label label-badge" style="font-weight:bold;">&middot;&middot;&middot;</span>
                 </span>
+                <span class="box-meta"><i class="fa fa-eye"></i>&nbsp;浏览：56</span>
+                <span class="box-meta"><i class="fa fa-clock-o"></i>&nbsp;发表：三天前</span>
             </div>
             <div class="box-view-btn-container">
                 <a href="#" class="btn btn-primary btn-md"><i class="fa fa-search"></i>&nbsp;阅读全文</a>
             </div>
         </div>
+
     </div>
+@endfor
 </div>
