9c6ea1d0e5d69e10e83d1e98744e35d405c59637
heqiming <messikiller@aliyun.com>
2016-12-21 18:56:50 +0800

    fix\

--- resources/views/home/sidebar.blade.php
+++ resources/views/home/sidebar.blade.php
@@ -12,7 +12,25 @@
 
     <div class="sidebar-box">
         <div class="sidebar-box-title">
-            <i class="fa fa-bookmark"></i>&nbsp;微语
+            <i class="fa fa-tags"></i>&nbsp;标签云
+        </div>
+        <div class="sidebar-box-divider"><div class="left"></div><div class="right"></div></div>
+        <div class="siderbar-box-content">
+            <a href="#" class="btn-tag">LAMP</a>
+            <a href="#" class="btn-tag">mysql</a>
+            <a href="#" class="btn-tag">mysql</a>
+            <a href="#" class="btn-tag">缓存</a>
+            <a href="#" class="btn-tag">反射API</a>
+            <a href="#" class="btn-tag">反射API</a>
+            <a href="#" class="btn-tag">mysql</a>
+            <a href="#" class="btn-tag">缓存</a>
+            <a href="#" class="btn-tag">反射API</a>
+        </div>
+    </div>
+
+    <div class="sidebar-box">
+        <div class="sidebar-box-title">
+            <i class="fa fa-list-ul"></i>&nbsp;文章分类
         </div>
         <div class="sidebar-box-divider"><div class="left"></div><div class="right"></div></div>
         <div class="siderbar-box-content">
@@ -22,7 +40,7 @@
 
     <div class="sidebar-box">
         <div class="sidebar-box-title">
-            <i class="fa fa-bookmark"></i>&nbsp;微语
+            <i class="fa fa-folder-open"></i>&nbsp;文章归档
         </div>
         <div class="sidebar-box-divider"><div class="left"></div><div class="right"></div></div>
         <div class="siderbar-box-content">
