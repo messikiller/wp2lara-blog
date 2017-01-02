24ad7861d174bec4c8b2bb4f7aa76d3d6f0954ac
messikiller <messikiller@aliyun.com>
2017-01-02 22:58:49 +0800

    fix

--- resources/views/admin/article_add.blade.php
+++ resources/views/admin/article_add.blade.php
@@ -5,17 +5,42 @@
 <div style="width:800px;">
 
     <form class="form-horizontal">
-      <div class="form-group">
-        <label for="input-title" class="col-sm-2">文章标题</label>
-        <div class="col-md-6 col-sm-10">
-          <input type="text" class="form-control" id="input-title">
+        <div class="form-group">
+          <label for="input-title" class="col-sm-2">文章标题</label>
+          <div class="col-md-6 col-sm-10">
+            <input type="text" class="form-control" id="input-title">
+          </div>
         </div>
-      </div>
+
+        <div class="form-group">
+          <label for="input-title" class="col-sm-2">分类</label>
+          <div class="col-md-6 col-sm-10">
+            <select class="form-control" id="input-cate">
+                <option value="0">请选择</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="input-title" class="col-sm-2">标签</label>
+          <div class="col-md-6 col-sm-10">
+            <select class="form-control" id="input-tag">
+                <option value="0">请选择</option>
+            </select>
+          </div>
+        </div>
 
       <div class="form-group">
         <label for="input-summary" class="col-sm-2">摘要</label>
         <div class="col-md-6 col-sm-10">
-            <textarea rows="8" cols="40" class="form-control" id="input-summary"></textarea>
+            <textarea rows="5" cols="40" class="form-control" id="input-summary"></textarea>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="input-summary" class="col-sm-2">内容</label>
+        <div class="col-md-6 col-sm-10">
+            <textarea rows="10" cols="40" class="form-control" id="input-summary"></textarea>
         </div>
       </div>
 
