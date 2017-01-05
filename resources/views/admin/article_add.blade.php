a99d0b707ad7c59c5690dba0f38426cc0649fd7b
messikiller <messikiller@aliyun.com>
2017-01-05 23:07:13 +0800

    fix

--- resources/views/admin/article_add.blade.php
+++ resources/views/admin/article_add.blade.php
@@ -7,19 +7,20 @@
     <li class="active">发布文章</li>
 </ol>
 
-<div style="width:800px;">
+<!-- <div style="width:800px;"> -->
+<div class="container-fluid">
 
     <form class="form-horizontal">
         <div class="form-group">
-          <label for="input-title" class="col-sm-2">文章标题</label>
-          <div class="col-md-6 col-sm-10">
+          <label for="input-title" class="col-sm-1">文章标题</label>
+          <div class="col-md-5">
             <input type="text" class="form-control" id="input-title">
           </div>
         </div>
 
         <div class="form-group">
-          <label for="input-title" class="col-sm-2">分类</label>
-          <div class="col-md-6 col-sm-10">
+          <label for="input-title" class="col-sm-1">分类</label>
+          <div class="col-md-5">
             <select class="form-control" id="input-cate">
                 <option value="0">请选择</option>
                 @foreach ($cates as $cate)
@@ -30,8 +31,8 @@
         </div>
 
         <div class="form-group">
-          <label for="input-title" class="col-sm-2">标签</label>
-          <div class="col-md-6 col-sm-10">
+          <label for="input-title" class="col-sm-1">标签</label>
+          <div class="col-md-5">
             <select class="form-control chosen-select" id="input-tag" multiple="true">
                 @foreach ($tags as $tag)
                 <option value="{{ $tag->Id }}">{{ $tag->name }}</option>
@@ -40,22 +41,26 @@
           </div>
         </div>
 
-      <div class="form-group">
-        <label for="input-summary" class="col-sm-2">摘要</label>
-        <div class="col-md-6 col-sm-10">
-            <textarea rows="5" cols="40" class="form-control" id="input-summary"></textarea>
+      <div class="form-group" style="height: 150px;">
+        <label for="input-summary" class="col-sm-1">摘要</label>
+        <div class="col-md-5" style="height: 100%;">
+            <textarea class="form-control" id="input-summary" oninput="preview_content(this.id, 'preview-summary')" style="height: 100%; resize: none;"></textarea>
         </div>
+        <div class="col-md-5" id="preview-summary" style="height: 100%;overflow-y: scroll;border: 1px dashed #555;"></div>
+      </div>
+
+      <div class="form-group" style="height: 500px;">
+        <label for="input-summary" class="col-sm-1">内容
+            <a href="#" class="btn btn-default btn-xs" id="max"><i class="fa fa-expand"></i></a>
+        </label>
+        <div class="col-md-5" style="height: 100%;">
+            <textarea class="form-control" id="input-content" oninput="preview_content(this.id, 'preview-content')" style="height: 100%; resize: none;"></textarea>
+        </div>
+        <div class="col-md-5" id="preview-content" style="height: 100%;overflow-y: scroll;border: 1px dashed #555;"></div>
       </div>
 
       <div class="form-group">
-        <label for="input-summary" class="col-sm-2">内容</label>
-        <div class="col-md-6 col-sm-10">
-            <textarea rows="10" cols="40" class="form-control" id="input-content" oninput="preview_content()"></textarea>
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
+        <div class="col-sm-offset-1 col-sm-10">
           <button type="submit" class="btn btn-success">提交</button>
         </div>
       </div>
@@ -63,21 +68,36 @@
 
 </div>
 
-<div id="preview"> </div>
-
-<div id="app">
-
-</div>
-
 @endsection
 
 @section('own-js')
-<script src="{{ asset('js/main.js') }}"></script>
 <script type="text/javascript">
-function preview_content()
+function preview_content(src_id, preview_id)
 {
-    var input = $('#input-content').val();
-    $('#preview-content').html(markdown.toHTML(input));
+    var input = $('#'+src_id).val();
+    $('#'+preview_id).html(markdown.toHTML(input));
 }
+
+var h_window = $(window).outerHeight();
+var w_window = $(window).outerWidth();
+
+$('#max').click(function(){
+    $('#input-content').css({
+        width: w_window / 2,
+        height: h_window,
+        position: 'fixed',
+        top: '0px',
+        left: '0px'
+    });
+
+    $('#preview-content').css({
+        width: w_window / 2,
+        height: h_window,
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        background: '#ffffff'
+    });
+});
 </script>
 @endsection
