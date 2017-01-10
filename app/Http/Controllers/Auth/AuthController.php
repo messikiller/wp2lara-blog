5ec55c033ee3d45be9d5e3d51381e6d1fa8fe2fe
heqiming <messikiller@aliyun.com>
2017-01-10 17:45:21 +0800

    reset auth middle ware for admin pages

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/articles';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new authentication controller instance.
