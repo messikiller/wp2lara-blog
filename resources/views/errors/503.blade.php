56053a1961ea9bd327407d9bad932e77bd9af6ac
heqiming <messikiller@aliyun.com>
2017-01-11 17:59:17 +0800

    update 503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Be right back.</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <style>
+            * {
+                font-family: "Helvetica Neue",Helvetica,Tahoma,Arial,'Microsoft Yahei','Hiragino Sans GB','WenQuanYi Micro Hei',sans-serif;
+            }
+
             html, body {
                 height: 100%;
             }
@@ -40,7 +45,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">网站正在维护中......</div>
             </div>
         </div>
     </body>
