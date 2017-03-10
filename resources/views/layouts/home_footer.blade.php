717aef711899ef475538bdc32c9305d19661707e
messikiller <messikiller@aliyun.com>
2017-03-10 17:18:11 +0800

    add umeng tongji code in home footer

--- resources/views/layouts/home_footer.blade.php
+++ resources/views/layouts/home_footer.blade.php
@@ -53,6 +53,7 @@
         <p>
             <img src="{{ asset('assets/images/countrylogo.png') }}" style="display:inline-block;width: 16px;">
             <a href="http://www.miitbeian.gov.cn" target="_blank">粤ICP备15117370号</a>
+	    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1261464132'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1261464132%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
         </p>
     </div>
 
